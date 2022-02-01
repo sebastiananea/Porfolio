777c850a22072b9ff941d2218069f1ef05bd6013
sebastiananea <sebastiananea@gmail.com>
2022-01-31 21:15:21 -0300

    texto a las tech

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,23 @@
-import { FaReact, FaCss3Alt, FaLinkedin } from 'react-icons/fa'
+import { FaReact, FaCss3Alt, FaLinkedin } from "react-icons/fa";
 import {
   DiNodejs,
   DiPostgresql,
   DiGithubBadge,
   DiJavascript1,
-} from 'react-icons/di'
-import { SiRedux, SiHtml5, SiWhatsapp } from 'react-icons/si'
-import { Button } from 'react-bootstrap'
-import 'animate.css'
-import wow from 'wowjs'
+} from "react-icons/di";
+import { SiRedux, SiHtml5, SiWhatsapp } from "react-icons/si";
+import { Button } from "react-bootstrap";
+import "animate.css";
+import wow from "wowjs";
+import s from "./About.module.css";
 
 export default function About() {
-  new wow.WOW().init()
+  new wow.WOW().init();
   return (
-    <div className='d-flex justify-content-around  m-5 pt-5 wow  animate__animated animate__fadeIn'>
-      <div className='text-center w-50'>
-        <h2 className='text-center m-3'>Sobre mi</h2>
-        <p className='m-3'>
+    <div className="d-flex justify-content-around  m-5 pt-5 wow  animate__animated animate__fadeIn">
+      <div className="text-center w-50">
+        <h2 className="text-center m-3">Sobre mi</h2>
+        <p className="m-4">
           Con formación en desarrollo full stack, y un fuerte interes en el
           arte, la ciencia, y el avance tecnológico como herramienta para hacer
           del mundo un lugar mejor. Estudié la Licenciatura en Higiene y
@@ -24,31 +25,55 @@ export default function About() {
           en Ciencias de la Computación. Me gusta tocar la guitarra, jugar
           videojuegos y practicar voley y futbol.
         </p>
-        <Button className='bg-dark'>
+        <Button className="bg-dark">
           Ver perfil
-          <FaLinkedin style={{ margin: 10 }} />
+          <FaLinkedin style={{ margin: 10, height: 35, width: 35 }} />
         </Button>
-        <Button className='bg-dark m-3'>
+        <Button className="bg-dark m-3">
           Contactar
-          <SiWhatsapp style={{ margin: 10 }} />
+          <SiWhatsapp style={{ margin: 10, height: 35, width: 35 }} />
         </Button>
       </div>
-      <div className='text-center w-50'>
-        <h2 className='text-center m-3'>Mi stack</h2>
+      <div className="text-center w-50">
+        <h2 className="text-center m-3">Mi stack</h2>
 
-        <div className='container text-center d-flex justify-content-around m-3'>
-          <SiHtml5 style={{ fontSize: 40 }} />
-          <FaCss3Alt style={{ fontSize: 40 }} />
-          <DiGithubBadge style={{ fontSize: 40 }} />
-          <DiJavascript1 style={{ fontSize: 40 }} />
+        <div className="container text-center d-flex justify-content-around mt-4 mb-3">
+          <span className={s.tech}>
+            <SiHtml5 />
+            <p>HTML</p>
+          </span>
+          <span className={s.tech}>
+            <FaCss3Alt style={{ fontSize: 40 }} />
+            <p>CSS</p>
+          </span>
+          <span className={s.tech}>
+            <DiGithubBadge style={{ fontSize: 40 }} />
+            <p>Github</p>
+          </span>
+          <span className={s.tech}>
+            <DiJavascript1 style={{ fontSize: 40 }} />
+            <p>Javascript</p>
+          </span>
         </div>
-        <div className='container text-center d-flex justify-content-around mt-5 m-3'>
-          <FaReact style={{ fontSize: 40 }} />
-          <DiNodejs style={{ fontSize: 40 }} />
-          <DiPostgresql style={{ fontSize: 40 }} />
-          <SiRedux style={{ fontSize: 40 }} />
+        <div className="container text-center d-flex justify-content-around mt-2 mb-3">
+          <span className={s.tech}>
+            <FaReact style={{ fontSize: 40 }} />
+            <p>React</p>
+          </span>
+          <span className={s.tech}>
+            <DiNodejs style={{ fontSize: 40 }} />
+            <p>Node.js</p>
+          </span>
+          <span className={s.tech}>
+            <DiPostgresql style={{ fontSize: 40 }} />
+            <p>PostgreSQL</p>
+          </span>
+          <span className={s.tech}>
+            <SiRedux style={{ fontSize: 40 }} />
+            <p>Redux</p>
+          </span>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/MyCards.js
+++ src/components/MyCards.js
@@ -1,12 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Row } from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Row } from "react-bootstrap";
 
-import { proj } from '../data'
-import MyCard from './MyCard'
+import { proj } from "../data";
+import MyCard from "./MyCard";
 
 export default function MyCards(props) {
   return (
-    <Row xs={1} md={3} className='g-4 mt-5 '>
+    <Row xs={1} md={3} className="g-4 mt-5 w-100">
       {proj.map(function (p) {
         return (
           <MyCard
@@ -16,8 +16,8 @@ export default function MyCards(props) {
             url={p.url}
             github={p.github}
           />
-        )
+        );
       })}
     </Row>
-  )
+  );
 }
